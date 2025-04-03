f80f8d1b74c1ffeead0d3457ff946bc44ed210ac
mohsen <mohsenrostamikia@gmail.com>
2025-04-03 11:55:05 +0330

    add jwt and authentication with jwt for users service

--- users-service/routes/api.php
+++ users-service/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class,'index'])
 //    ->middleware('auth:sanctum')
 ;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->get('/profile', [AuthController::class, 'profile']);
