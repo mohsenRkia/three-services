522b9c1adab0977ec3da6c612ccb91adff92124b
mohsen <mohsenrostamikia@gmail.com>
2025-08-21 18:04:00 +0330

    remove Query from ddd structure

--- products-service/src/Delivery/Http/Product/Handlers/GetProductHandler.php
+++ products-service/src/Delivery/Http/Product/Handlers/GetProductHandler.php
@@ -2,7 +2,6 @@
 
 namespace src\Delivery\Http\Product\Handlers;
 
-use src\Application\Product\Queries\GetProduct;
 use src\Application\Product\DTO\ProductDTO;
 use src\Application\Product\Services\ProductService;
 use src\Domain\Product\ValueObjects\ProductId;
